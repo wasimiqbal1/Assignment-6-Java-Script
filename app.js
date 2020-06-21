17633732734b73ad71ec31deab27391f9142bdaa
wasimiqbal1 <wasim_iqbal@msn.com>
2020-06-21 17:01:53 +0500

    Updated

---
+++ app.js
@@ -0,0 +1,12 @@
+// ==== Chapter-21-25 & Task-1
+/*var firstName = prompt("Enter your first name", "Your first name is:");
+var lastName = prompt("Enter your last name", "Your last name is:");
+var fullName = firstName + " " + lastName;
+alert(fullName);*/
+
+// ==== Chapter-21-25 & Task-2
+/*var a = prompt("Enter your favorite mobile phone model");
+var a = ("Samsung Galaxy S6 Edge plus ");
+document.write("My favorite phone is: " + a + "<br>" + "Length of string is: " + a.length);*/
+
+// ==== Chapter-21-25 & Task-3
