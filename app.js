fce6faae0d340c8071b42a68661bcf9b1675d661
wasimiqbal1 <wasim_iqbal@msn.com>
2020-06-21 17:07:40 +0500

    Update

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ var a = ("Samsung Galaxy S6 Edge plus ");
 document.write("My favorite phone is: " + a + "<br>" + "Length of string is: " + a.length);*/
 
 // ==== Chapter-21-25 & Task-3
+/*var a = ("Pakistan");
+document.write("String: " + a + "<br>" + "index of 'n': " + a.indexOf("n"));*/
+
+// ==== Chapter-21-25 & Task-4
+/*var a = "Hello World";
+document.write("String: " + a + "<br>");
+var b = a.lastIndexOf("l");
+document.write("Last index of 'l': " + b);*/
+
+// ==== Chapter-21-25 & Task-5
+/*var a = "Pakistan";
+document.write("String: " + a + "<br>" + "Character at index 3: " + a.charAt("3"));*/
